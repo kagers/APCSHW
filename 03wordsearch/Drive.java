91fc48e2bd1bf23ea43cc85712c3377902fc2894
kagers <ka.gershfeld@gmail.com>
2014-11-19 21:37:13 -0500

    die-agonal

--- 03wordsearch/Drive.java
+++ 03wordsearch/Drive.java
@@ -2,5 +2,12 @@ public class Drive{
     public static void main(String[]args){
 	WordGrid g = new WordGrid(5,5);
 	System.out.println(g);
+	System.out.println(g.addWordHorizontal("word",6,6));
+	System.out.println(g.addWordHorizontal("hyper",2,0));
+        System.out.println(g.addWordVertical("geom",3,3));
+	System.out.println(g.addWordVertical("geom",1,3));
+	System.out.println(g.addWordDiagonal("tyfo",0,0));
+	System.out.println(g.addWordDiagonal("typo",0,0));
+	System.out.println(g);
     }
 }
--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -45,8 +45,84 @@ public class WordGrid{
      *@return true when the word is added successfully. When the word doesn't fit,
      *or there are overlapping letters that do not match, then false is returned.
      */
-    //public boolean addWordHorizontal(String word,int row, int col){
-    //}
-    
-    //vertical + diagonal should be implemented as well.
+    public boolean addWordHorizontal(String word,int row, int col){
+	if (word.length()>data[0].length || row>=data.length || col>=data[0].length){
+		return false;
+	}
+	int x=col;
+	for (int i=0;i<word.length();i++){
+		if (x>=data[0].length || (data[row][x]!='_' && word.charAt(i)!=data[row][x])){
+			return false;
+		}
+		x++;
+	}
+	for (int i=0;i<word.length();i++){
+                data[row][col]=word.charAt(i);
+                col++;
+        }
+	return true;
+    }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from top to bottom, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    public boolean addWordVertical(String word,int row, int col){
+        if (word.length()>data.length || row>=data.length || col>=data[0].length){
+                return false;
+        }
+        int y=row;
+        for (int i=0;i<word.length();i++){
+                if (y>=data.length || (data[y][col]!='_' && word.charAt(i)!=data[y][col])){
+                        return false;
+                }
+                y++;
+        }
+        for (int i=0;i<word.length();i++){
+                data[row][col]=word.charAt(i);
+                row++;
+        }
+        return true;
+    }
+
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added diagonally from top left to bottom right, must fit on
+     *the WordGrid, and must have a corresponding letter to match any letters
+     *that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    public boolean addWordDiagonal(String word,int row, int col){
+        if (word.length()>(Math.min(data[0].length,data.length)) || row>=data.length || col>=data[0].length){
+                return false;
+        }
+        int x=col;
+	int y=row;
+        for (int i=0;i<word.length();i++){
+                if (x>=data[0].length || y>=data.length || (data[y][x]!='_' && word.charAt(i)!=data[y][x])){
+                        return false;
+                }
+                x++;
+		y++;
+        }
+        for (int i=0;i<word.length();i++){
+                data[row][col]=word.charAt(i);
+                col++;
+		row++;
+        }
+        return true;
+    }
+
+
 }
