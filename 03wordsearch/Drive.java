efbdabd3fd7634f8237122ea2bf7d8e02f8b8579
kagers <Ka.gershfeld@gmail.com>
2014-11-22 16:31:23 -0500

    file reading

--- 03wordsearch/Drive.java
+++ 03wordsearch/Drive.java
@@ -1,5 +1,8 @@
+import java.util.*;
+import java.io.*;
+
 public class Drive{
-    public static void main(String[]args){
+    public static void main(String[]args) throws FileNotFoundException{
 	WordGrid g = new WordGrid(5,5);
 	System.out.println(g);
 	System.out.println(g.addWord("word",6,6,0,1));
@@ -9,5 +12,12 @@ public class Drive{
 	System.out.println(g.addWord("tyfo",0,0,1,1));
 	System.out.println(g.addWord("typo",0,0,1,1));
 	System.out.println(g);
+	File words = new File("words.txt");
+	Scanner scan = new Scanner(words);
+	ArrayList<String> slova = new ArrayList<String>();
+	while(scan.hasNextLine()){
+	    slova.add(scan.nextLine());
+	}
+	System.out.println(slova);
     }
 }
