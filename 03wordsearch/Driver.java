c897971b6e4cd29993a66c06b2c373c7b31e1342
kagers <Ka.gershfeld@gmail.com>
2014-11-24 19:55:59 -0500

    rename

---
+++ 03wordsearch/Driver.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver{
+    public static void main(String[]args) throws FileNotFoundException{
+	WordGrid g = new WordGrid(15,15);
+	File mots = new File("words.txt");
+	Scanner scan = new Scanner(mots);
+	ArrayList<String> slova = new ArrayList<String>();
+	while(scan.hasNextLine()){
+	    slova.add(scan.nextLine());
+	}
+	g.generate(slova);
+	System.out.println(g);
+    }
+}
