cf1c1bcea51b21b840d4b12cac517d1a7bea87bb
kagers <Ka.gershfeld@gmail.com>
2014-11-24 20:29:40 -0500

    K words

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Driver{
     public static void main(String[]args) throws FileNotFoundException{
-	WordGrid g = new WordGrid(15,15);
+	WordGrid g = new WordGrid(35,35);
 	File mots = new File("words.txt");
 	Scanner scan = new Scanner(mots);
 	ArrayList<String> slova = new ArrayList<String>();
