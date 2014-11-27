0cd1e52916a75eec11b0158d5e6e203a5a01a0f5
Katherine Gershfeld <ka.gershfeld@gmail.com>
2014-11-27 15:09:56 -0500

    started user input though there's probably a better way

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -3,10 +3,33 @@ import java.io.*;
 
 public class Driver{
     public static void main(String[]args) throws FileNotFoundException{
-	WordGrid w = new WordGrid(10,10);
-	w.setSeed(11);
-	w.loadWordsFromFile("words.txt",false);
-	System.out.println(w);
-	System.out.println(w.wordsInPuzzle());
+	if (args.length==2){
+	    WordGrid w = new WordGrid(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+	    w.loadWordsFromFile("words.txt",true);
+	    System.out.println(w.wordsInPuzzle());
+	    System.out.println(w);
+	}
+	else if (args.length==3){
+	   WordGrid w = new WordGrid(Integer.parseInt(args[0]),Integer.parseInt(args[1])); 
+	    w.setSeed(Integer.parseInt(args[2]));
+	    w.loadWordsFromFile("words.txt",true);
+	    System.out.println(w.wordsInPuzzle());
+	    System.out.println(w);
+	}
+	else if (args.length==4){
+	    WordGrid w = new WordGrid(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+	    w.setSeed(Integer.parseInt(args[2]));
+	    boolean b=true;
+	    if (args[3].equals("1")){
+		b=false;
+	    }
+	    w.loadWordsFromFile("words.txt",b);		
+	    System.out.println(w.wordsInPuzzle());
+	    System.out.println(w);
+	}
+	else{
+	    System.out.println("Input arguments in the format:");
+	    System.out.println("java Driver rows cols [randomSeed [answers]]");
+	}
     }
 }
