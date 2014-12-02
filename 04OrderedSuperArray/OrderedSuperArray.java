81042d565aad7f8e55168d5b7b0ccb44e1f5f680
Katherine Gershfeld <ka.gershfeld@gmail.com>
2014-12-01 19:09:47 -0500

    add actually adds

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -8,17 +8,37 @@ public class OrderedSuperArray extends SuperArray{
 	}
 	else {
 	    int i=0;
-	    while ((i<size())||(get(i).compareTo(e)<=0)){
+	    while ((i<size())&&(get(i).compareTo(e)<=0)){
 		i++;
 	    }
-	    super.add(i,e);
+	    if (i==size()){
+		super.add(e);
+	    }
+	    else {
+		super.add(i,e);
+	    }
 	}
+    }
+    public void add(int index, String o){
+	this.add(o);
     }
     public static void main(String[]args){
 	OrderedSuperArray s = new OrderedSuperArray(10);
-	s.add("a");
+	s.add("bourgeoisie");
 	System.out.println(s);
-	s.add("b");
+	s.add("android");
+	System.out.println(s);
+	s.add("paranoid");
+	System.out.println(s);
+	s.add("duran");
+	System.out.println(s);
+	s.add("bohr");
+	System.out.println(s);
+	s.add("bond");
+	System.out.println(s);
+	s.add("duran");
+	System.out.println(s);
+	s.add(26000,"Radio");
 	System.out.println(s);
     }
 }
