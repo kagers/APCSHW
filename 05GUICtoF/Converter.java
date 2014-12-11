697451e8cc131a0ed54e13b5ea8f847cee6b6139
Katherine Gershfeld <ka.gershfeld@gmail.com>
2014-12-10 20:20:58 -0500

    warm GUI center

--- 05GUICtoF/Converter.java
+++ 05GUICtoF/Converter.java
@@ -3,20 +3,66 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Converter extends JFrame implements ActionListener{
-    private container pane;
+    private Container pane,io,but;
     private JButton b;
+    private JRadioButton c,f;
+    private JTextField text;
+    private JLabel info,output;
+
     public Converter() {
-        this.setTitle("Fahrenheit to Celsius");
-        this.setSize(400,200);
+        this.setTitle("Temperature Converter");
+        this.setSize(500,200);
         this.setLocation(100,100);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	
 	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+	pane.setLayout(new BoxLayout(pane,BoxLayout.Y_AXIS));
+
 	b = new JButton("Convert!");
+	b.setActionCommand("convert");
+	b.addActionListener(this);
+	
+	text = new JTextField(10);
+	output = new JLabel("<html>Input a number into the text field <br> Feel the  mathgic happen</html>",JLabel.CENTER);
+
+	c = new JRadioButton("to Celsius",true);
+	f = new JRadioButton("to Fahrenheit");
+	
+	ButtonGroup unit = new ButtonGroup();
+	unit.add(c);
+	unit.add(f);
+
+	io = new Container();
+	io.setLayout(new FlowLayout());
+	io.add(text);
+	io.add(output);
+
+	but = new Container();
+	but.setLayout(new FlowLayout());
+	but.add(c);
+	but.add(f);
+	but.add(b);
+
+	pane.add(io);
+	pane.add(but);
+
     }
     public void actionPerformed(ActionEvent e){
-
+	String action = e.getActionCommand();
+	if (action.equals("convert")){
+	    try {
+		double s = Double.parseDouble(text.getText());
+		if (c.isSelected()){
+		    output.setText(((5/9.0)*(s-32))+" C");
+		}
+		else{
+		    output.setText((((9/5.0)*s)+32)+" F");
+		}
+	    }
+	    catch (Exception f){
+		output.setText("Sure about that?");
+	    }
+	}
     }
     public static void main(String[] args) {
         Converter g = new Converter();
